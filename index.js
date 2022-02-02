ace0be1e4ac90e033cec3eb172844ba3ef2a476b
Drew Grey <dtsgrey@gmail.com>
2022-02-02 14:54:52 -0600

    commit tree d3affe9365df372666decc44a39fe3f1081884d5

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+   return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function fareMultiplier(fare) {
+        return int * fare; 
+    }
+}
+
+function fareDoubler(fare){
+   const cost = createFareMultiplier(2);
+   return cost(fare);
+} 
+
+function fareTripler(fare){
+    const cost = createFareMultiplier(3);
+    return cost(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+        //console.log(func)
+        if(func === returnFirstTwoDrivers){
+            return returnFirstTwoDrivers(arrayOfDrivers)
+        }
+        else if(func === returnLastTwoDrivers){
+           return returnLastTwoDrivers(arrayOfDrivers);
+        }
+        else{
+            return "Please enter a function."
+        }
+        
+}
